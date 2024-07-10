88e50dc41726c7d0d2497724dfc61b47d3cc2b42
midnite-ops <121711700+midnite-ops@users.noreply.github.com>
2024-07-10 14:42:37 +0100

    New cards added

--- js-files/js-card-img.js
+++ js-files/js-card-img.js
@@ -1,1 +1,157 @@
-const cards = []
+const cards = [{
+    card: `Assets/cards/diamond-ace.jpg`,
+    value: 11
+},{
+    card: `Assets/cards/diamond-2.jpg`,
+    value: 2
+},{
+    card: `Assets/cards/diamond-3.jpg`,
+    value: 3
+},{
+    card: `Assets/cards/diamond-4.jpg`,
+    value: 4
+},{
+    card: `Assets/cards/diamond-5.jpg`,
+    value: 5
+},{
+    card: `Assets/cards/diamond-6.jpg`,
+    value: 6
+},{
+    card: `Assets/cards/diamond-7.jpg`,
+    value: 7
+},{
+    card: `Assets/cards/diamond-8.jpg`,
+    value: 8
+},{
+    card: `Assets/cards/diamond-9.jpg`,
+    value: 9
+},{
+    card: `Assets/cards/diamond-10.jpg`,
+    value: 10
+},{
+    card: `Assets/cards/diamond-J.jpg`,
+    value: 10
+},{
+    card: `Assets/cards/diamond-Q.jpg`,
+    value: 10
+},{
+    card: `Assets/cards/diamond-K.jpg`,
+    value: 10
+},{
+    card: `Assets/cards/love-ace.jpg`,
+    value: 11
+},{
+    card: `Assets/cards/love-2.jpg`,
+    value: 2
+},{
+    card: `Assets/cards/love-3.jpg`,
+    value: 3
+},{
+    card: `Assets/cards/love-4.jpg`,
+    value: 4
+},{
+    card: `Assets/cards/love-5.jpg`,
+    value: 5
+},{
+    card: `Assets/cards/love-6.jpg`,
+    value: 6
+},{
+    card: `Assets/cards/love-7.jpg`,
+    value: 7
+},{
+    card: `Assets/cards/love-8.jpg`,
+    value: 8
+},{
+    card: `Assets/cards/love-9.jpg`,
+    value: 9
+},{
+    card: `Assets/cards/love-10.jpg`,
+    value: 10
+},{
+    card: `Assets/cards/love-J.jpg`,
+    value: 10
+},{
+    card: `Assets/cards/love-Q.jpg`,
+    value: 10
+},{
+    card: `Assets/cards/love-K.jpg`,
+    value: 10
+},{
+    card: `Assets/cards/spade-ace.jpg`,
+    value: 11
+},{
+    card: `Assets/cards/spade-2.jpg`,
+    value: 2
+},{
+    card: `Assets/cards/spade-3.jpg`,
+    value: 3
+},{
+    card: `Assets/cards/spade-4.jpg`,
+    value: 4
+},{
+    card: `Assets/cards/spade-5.jpg`,
+    value: 5
+},{
+    card: `Assets/cards/spade-6.jpg`,
+    value: 6
+},{
+    card: `Assets/cards/spade-7.jpg`,
+    value: 7
+},{
+    card: `Assets/cards/spade-8.jpg`,
+    value: 8
+},{
+    card: `Assets/cards/spade-9.jpg`,
+    value: 9
+},{
+    card: `Assets/cards/spade-10.jpg`,
+    value: 10
+},{
+    card: `Assets/cards/spade-J.jpg`,
+    value: 10
+},{
+    card: `Assets/cards/spade-Q.jpg`,
+    value: 10
+},{
+    card: `Assets/cards/spade-K.jpg`,
+    value: 10
+},{
+    card: `Assets/cards/tree-ace.jpg`,
+    value: 11
+},{
+    card: `Assets/cards/tree-2.jpg`,
+    value: 2
+},{
+    card: `Assets/cards/tree-3.jpg`,
+    value: 3
+},{
+    card: `Assets/cards/tree-4.jpg`,
+    value: 4
+},{
+    card: `Assets/cards/tree-5.jpg`,
+    value: 5
+},{
+    card: `Assets/cards/tree-6.jpg`,
+    value: 6
+},{
+    card: `Assets/cards/tree-7.jpg`,
+    value: 7
+},{
+    card: `Assets/cards/tree-8.jpg`,
+    value: 8
+},{
+    card: `Assets/cards/tree-9.jpg`,
+    value: 9
+},{
+    card: `Assets/cards/tree-10.jpg`,
+    value: 10
+},{
+    card: `Assets/cards/tree-J.jpg`,
+    value: 10
+},{
+    card: `Assets/cards/tree-Q.jpg`,
+    value: 10
+},{
+    card: `Assets/cards/tree-K.jpg`,
+    value: 10
+}]
